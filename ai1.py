66e4b979c06b605f21036f4de5d4da0ddcf66416
Pratham Kamath <prathamkamath627@gmail.com>
2026-05-23 19:08:32 +0530

    Add files via upload

---
+++ ai1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a choice (1 for sunny, 2 for rainy): "))
+if a == 1:
+    print("Wear sunglasses")
+elif a == 2:
+    print("Take an umbrella")
+else:
+    print("not defind")
+
---
+++ ai2.py
@@ -0,0 +1,7 @@
+temp = float(input("Enter the temperature in °C: "))
+if temp > 30:
+    print("Drink plenty of water")
+elif temp > 20:
+    print("Weather is pleasant")
+else:
+    print("Wear a light jacket")
