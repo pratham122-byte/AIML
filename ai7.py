f8494a5785c6d7552945ed7ef3c1c832f1ea7ac6
Pratham Kamath <prathamkamath627@gmail.com>
2026-05-25 19:43:31 +0530

    Add files via upload

---
+++ ai7.py
@@ -0,0 +1,9 @@
+a = int(input("enter what the  weather is today(sunny=3,cloudly=2,rainy=1)"))
+if a ==1:
+    print("carry a rain jacket")
+elif a ==2:
+    print("carry a umbrella")
+elif a ==3:
+    print("wear light weight jacket")
+else:
+    print("invaild weather input")
